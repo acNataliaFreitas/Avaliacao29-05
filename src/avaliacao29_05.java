f9cf8b1731e5304a285023c2446c6d91874e9720
Natália Freitas <natyff1@hotmail.com>
2020-05-29 19:34:14 -0300

    Leitura dos dados

--- src/avaliacao29_05.java
+++ src/avaliacao29_05.java
@@ -1,9 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class avaliacao29_05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		String nome;
+		double horasTrabalhadas;
+		char opcao='s';
+		
+		//LEITURA DE DADOS 
+		System.out.print("Nome: ");
+		nome=sc.next();
+		System.out.println("Horas trabalhadas: ");
+		horasTrabalhadas=sc.nextDouble();
+		System.out.println("Digitar outro (S/N)? ");
+		nome=sc.next();
+		
+		while( opcao == 's'){
+			System.out.print("Nome: ");
+			nome=sc.next();
+			System.out.println("Horas trabalhadas: ");
+			horasTrabalhadas=sc.nextDouble();
+			System.out.println("Digitar outro (S/N)? ");
+			nome=sc.next();
+		}
+		
+		
+		sc.close();
 	}
 
 }
